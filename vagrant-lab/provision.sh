ddc94b548e4fee91d2f251a6a800ff8b3a6ffd27
Anderson Vieira Machado <andersonvm1@gmail.com>
2023-11-02 20:00:43 -0300

    Correção no Vagrant File e correção do plugin vagrant-vbguest

--- vagrant-lab/provision.sh
+++ vagrant-lab/provision.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing Apache and setting it up..."
+sudo yum -y update kernel
 yum install -y httpd >/dev/null 2>&1
-cp -r /vagrant/html/* /var/www/thml/
+cp -r /vagrant/html/* /var/www/html/
 service httpd start
