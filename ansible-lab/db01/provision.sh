0e07f52d33d4f623502465a13755207532c7145e
Anderson Vieira Machado <andersonvm1@gmail.com>
2023-11-16 16:30:39 -0300

    Criação dos playbooks

---
+++ ansible-lab/db01/provision.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCufE33FQZ9DQLLNGMU/Uso89Lh6lDdsBByiaO8a2sBWz8cTZ622YwTehZXio2Ay0s1GXIOheeoTmobuvqcqUp739wB2L7rRUTrpLO5/McjU8cvO8GObYZpz0qkdrMDj0krw8/8O3c7r6wA+3yGm6NS0f3gLmhtuVsCk+smoQ/UWYDRqQkI9TUpaH042kuklI3/UIndecw9QlIeuNKVgNpzl0+VWsLM4f0h6/QNud1ZDBrszORchdDXnR5RFFQeqSIlQczruJDebB5QTV4vn408Sa3F1Aia0fwNLMJmq+Uu/rLsKB/XgGLnh9F+2yxhK2NdHl/EfJbzKqh7KqWtfLwb vagrant@control-node
+EOT
